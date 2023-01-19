9e4775e567ca2aee81a27f89920942dd7c096e01
Anneliseishere <peter.capcara@gmail.com>
2023-01-19 15:53:59 +0100

    solved task1

--- task1/task1.py
+++ task1/task1.py
@@ -1,7 +1,7 @@
 with open("in.txt") as f:
     content = f.read().strip()
 numbers = [int(i) for i in content.split(",")]
-max_number = None
+max_number = max(numbers)
 # TODO: Find max element in numbers array
 
 print(max_number)

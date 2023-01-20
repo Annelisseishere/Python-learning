570207a711c9cadf4ebd393154ec7d7d5ef366c2
Anneliseishere <peter.capcara@gmail.com>
2023-01-20 13:22:57 +0100

    task1 solved while approach

--- task1/task1.py
+++ task1/task1.py
@@ -1,7 +1,30 @@
 with open("in.txt") as f:
     content = f.read().strip()
 numbers = [int(i) for i in content.split(",")]
-max_number = max(numbers)
+
 # TODO: Find max element in numbers array
 
-print(max_number)
+max_number = numbers[0]
+min_number = max_number
+
+i=1
+while i < len(numbers):
+    # find min value
+    if min_number > numbers[i]:
+        min_number = numbers[i]
+    # find max value
+    if max_number < numbers[i]:
+        max_number = numbers[i]
+    # increment, could be i += 1
+    i = i + 1
+
+# print min
+print("MIN VALUE: ", min_number)
+
+# print max
+print("MAX VALUE: ", max_number)
+
+
+    
+
+

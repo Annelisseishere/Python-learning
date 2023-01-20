e44d28c7c7d3f2b3f468805357ff1d04cd1a60ff
Jakub Lichman <lichman.jakub@gmail.com>
2023-01-20 18:38:43 +0000

    Task2 added

---
+++ task2/task2.py
@@ -0,0 +1,18 @@
+with open("in.txt") as f:
+    content = f.read().strip()
+numbers = [int(i) for i in content.split(",")]
+
+def closest(numbers, target):
+    result = None
+    # TODO: find closest number to target (can be equal to target)
+
+    return result
+
+# print 
+print("CLOSEST VALUE: ", closest(numbers, 23))
+
+
+
+    
+
+

cddeb45a20920fe7c24101961f43558a43a76fa5
Anneliseishere <peter.capcara@gmail.com>
2023-01-24 18:42:07 +0100

    task2solved

--- task2/task2.py
+++ task2/task2.py
@@ -3,13 +3,35 @@ with open("in.txt") as f:
 numbers = [int(i) for i in content.split(",")]
 
 def closest(numbers, target):
-    result = None
+    result = numbers[0]
+    i=1
+    while i < len(numbers):
+        if target == result:
+            result = target
+            break
+        else:
+            min_value = abs(target - result)
+            #print(target, numbers[i])
+            if target == numbers[i]:
+                result = numbers[i]
+                break
+            else:
+                temp = abs(target - numbers[i])
+                #print(numbers[i], temp, min_value)
+                if temp < min_value:
+                    min_value = temp
+                    result = numbers[i]
+
+        i = i + 1
+
+
     # TODO: find closest number to target (can be equal to target)
+
 
     return result
 
 # print 
-print("CLOSEST VALUE: ", closest(numbers, 23))
+print("CLOSEST VALUE: ", closest(numbers, 25))
 
 
 

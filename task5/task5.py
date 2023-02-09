5bbe7c33c48a902182c0693386ed9780ccd747bc
Anneliseishere <peter.capcara@gmail.com>
2023-02-09 13:29:59 +0100

    task5 solved

--- task5/task5.py
+++ task5/task5.py
@@ -4,8 +4,18 @@ numbers = [int(i) for i in content.split(",")]
 
 def remove_duplicates(numbers):
 	# given numbers sorted in ascending order, remove all duplicates in-place i.e. you are allowed to only modify the array not create new one
+    i = 0
+    for j in range(0, len(numbers)-1):
+        if numbers[i] == numbers[i+1]:
+            numbers.remove(numbers[i])
+            numbers.insert(len(numbers), None)
+        else:
+            i = i+1
+        
+            
+                
+
     return numbers
- 
 #  print result
 print("removed: ", remove_duplicates(sorted(numbers)))
 

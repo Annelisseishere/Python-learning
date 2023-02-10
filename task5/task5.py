520b8c1422989b2077ba67294e84460af657e789
Jakub Lichman <lichman.jakub@gmail.com>
2023-02-10 10:28:58 +0100

    Few comments on task5

--- task5/task5.py
+++ task5/task5.py
@@ -6,7 +6,9 @@ def remove_duplicates(numbers):
 	# given numbers sorted in ascending order, remove all duplicates in-place i.e. you are allowed to only modify the array not create new one
     i = 0
     for j in range(0, len(numbers)-1):
+		# This condition is good
         if numbers[i] == numbers[i+1]:
+			# don't remove and insert here but shift numbers[i+2] to numbers[i+1]
             numbers.remove(numbers[i])
             numbers.insert(len(numbers), None)
         else:

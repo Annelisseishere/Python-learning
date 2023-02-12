b1b5bb00a4e8c1589efd8288d043afd43a3b638d
Jakub Lichman <lichman.jakub@gmail.com>
2023-02-12 23:27:06 +0100

    Task6 added

---
+++ task6/task6.py
@@ -0,0 +1,10 @@
+
+def increment(number):
+	# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+	# The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+	# Increment the large integer by one and return the resulting array of digits.                
+
+	return number
+
+#  print the result. [1, 1, 0, 0] is expected.
+print("incremented: ", increment([1, 0, 9, 9]))

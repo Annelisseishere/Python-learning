bbd8de45462b5bfeec698fdce1e46087d017a771
Jakub Lichman <lichman.jakub@gmail.com>
2023-02-20 23:43:38 +0100

    Task7 added

---
+++ task7/task7.py
@@ -0,0 +1,8 @@
+
+def increment(number1, number2):
+	# Given 2 numbers represented as array of digits (same as in the previous task) perform addition between the two and return resulting array.
+
+	return number1
+
+#  print the result. [1, 1, 9, 8] is expected.
+print("added: ", increment([1, 0, 9, 9], [9, 9]))

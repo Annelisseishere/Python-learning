dfcd60dea8ec4fc2ef44276a67b0ac1d93af6ab3
Jakub Lichman <lichman.jakub@gmail.com>
2023-03-03 18:53:06 +0100

    Task8 added

---
+++ task8/task8.py
@@ -0,0 +1,8 @@
+strings = ["flower","flow","flight"]
+
+def longest_common_prefix(strings):
+	# TODO: compute a longest common prefix of the strings provided. In other words, you need to find a longest string which appears at the begging of each string in strings.
+	return ''
+
+# Should print 'fl'
+print(longest_common_prefix(strings))

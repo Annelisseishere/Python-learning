5ef3373a6614bda0b0c8aca8e8dc46db73d8ea23
Anneliseishere <peter.capcara@gmail.com>
2023-03-06 20:53:02 +0100

    task8 solved

--- task8/task8.py
+++ task8/task8.py
@@ -2,7 +2,16 @@ strings = ["flower","flow","flight"]
 
 def longest_common_prefix(strings):
 	# TODO: compute a longest common prefix of the strings provided. In other words, you need to find a longest string which appears at the begging of each string in strings.
-	return ''
+	prefix = ""
+	for i,ch in enumerate(strings[0]):
+		for j in range(1, len(strings)):
+			if i >= len(strings[j]):
+				return prefix
+			if ch != strings[j][i]:
+				return prefix
+		prefix += ch
+
+	return prefix
 
 # Should print 'fl'
 print(longest_common_prefix(strings))

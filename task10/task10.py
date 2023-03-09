c0b08bbe368ec75aa50af4d64735ba1d80d07364
Jakub Lichman <lichman.jakub@gmail.com>
2023-03-09 21:59:01 +0100

    Task10 added

---
+++ task10/task10.py
@@ -0,0 +1,7 @@
+def lengthOfLongestSubstring(s):
+	# TODO: Given a string s, find the length of the longest substring without repeating characters.
+	return 0
+
+print(lengthOfLongestSubstring("abcabcbb")) # 3
+print(lengthOfLongestSubstring("bbbbb")) # 1
+print(lengthOfLongestSubstring("pwwkew")) # 3

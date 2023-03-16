1268dba8b44de1f1b75ea3220220035c187f2ed7
Jakub Lichman <lichman.jakub@gmail.com>
2023-03-16 22:40:24 +0100

    Task11 added

---
+++ task11/task11.py
@@ -0,0 +1,9 @@
+def valid_paranthesis(s):
+	# given string s containing alpha numberical characters + '(', '[', '{', '}', ']', ')'
+	# return True if all brackets are matched, False otherwise
+	return False
+
+
+print(valid_paranthesis("(sfdsdf{asdasd}[asdasd])"))  # True
+print(valid_paranthesis("(asdadsa s(asdad a)]")) # False
+# TODO: write at least 5 more tests"
